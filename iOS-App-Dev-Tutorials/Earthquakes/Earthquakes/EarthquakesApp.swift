6c2871381f27337a400958e16f6d2b82c4a4f85c
Tamás Scheuring <102406235+scheuringtamas@users.noreply.github.com>
2024-03-01 15:07:10 +0100

    Add files via upload

---
+++ iOS-App-Dev-Tutorials/Earthquakes/Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,19 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+The app and main window group scene.
+*/
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    @StateObject var quakesProvider = QuakesProvider()
+    var body: some Scene {
+        WindowGroup {
+            Quakes()
+                .environmentObject(quakesProvider)
+        }
+    }
+}
